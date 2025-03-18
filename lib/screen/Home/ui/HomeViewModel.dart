a05e8b3f37282904adbc136af73a548bbf8795e5
DataBase18 <aramireza18@miumg.edu.gt>
2025-03-17 21:34:10 -0600

    Se arregala la regex y a ya lee cadenas indivuales y en estrucutrea or, solo falta revisar que termien en nada y empieze en nada

--- lib/screen/Home/ui/HomeViewModel.dart
+++ lib/screen/Home/ui/HomeViewModel.dart
@@ -13,4 +13,51 @@ class HomeViewModel extends EventViewModel {
     notify(ChangeLoadingScreen(newState));
   }
 
+  void compile (String code){
+    String regexVariableName = r"\s*(\w+)\s*=(.*)";
+    String regexValueWithApostrophe = r"\s*'(.*)'\s*";
+    String regexValueWithoutApostrophe = r"\s*([A-Za-z0-9_-]+)\s*";
+    
+
+    final variablesRegexCompile = RegExp(regexVariableName);
+    Iterable<RegExpMatch> variablesMatches = variablesRegexCompile.allMatches(code);
+
+
+    String regexOrPart = "(\s*($regexValueWithApostrophe)|($regexValueWithoutApostrophe))(\\|(($regexValueWithApostrophe)|($regexValueWithoutApostrophe)))+";
+    String regexOnlyVarPart = "($regexValueWithApostrophe)|($regexValueWithoutApostrophe)";
+    String valueVar ;
+    String varName ;
+
+    // 4 PRIMER VAIRABLE CCON APOSTROFE
+    // 6 PRIMER VAIRABLE SIN APOSTROFE
+    // 12 REGEX EN OR SIN APOSTROFE
+    // 15 variable sola con apostrofe
+    // 17 variable sola sin apostrofe
+
+    for(var matchVariable in variablesMatches){
+      valueVar = matchVariable.group(2)??"";
+      varName = matchVariable.group(1)??"";
+
+      //Find                                            13
+      final varsValueMatches =  RegExp("($regexOrPart)|($regexOnlyVarPart)");
+      final matchesValues = varsValueMatches.allMatches(valueVar);
+      for(var match in matchesValues){
+
+        if(match.group(1) != null){
+          print("Resto de condicionales:   ${match.group(1) }");
+        }
+        if(match.group(15) != null){
+          print("Variable sola con apostrofe: ${match.group(15) }");
+        }
+        if(match.group(17) != null){
+          print("Variable sola SIN apostrofe: ${match.group(17) }");
+        }
+
+      }
+
+    }
+
+
+  }
+
 }
--- lib/screen/Home/ui/HomeWidget.dart
+++ lib/screen/Home/ui/HomeWidget.dart
@@ -3,6 +3,7 @@ import 'package:compiladorestareauno/widget/InputBasic.dart';
 import 'package:flutter/material.dart';
 import 'package:compiladorestareauno/screen/Home/ui/HomeState.dart';
 import 'package:compiladorestareauno/screen/Home/ui/HomeViewModel.dart';
+import 'package:flutter/services.dart';
 
 
 class HomeBody extends StatelessWidget {
@@ -16,56 +17,67 @@ class HomeBody extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          IconButton(
-            onPressed: (){
-
-            },
-            icon:  IntrinsicWidth(
-              child:   Row(
-                children: [
-                  const Icon(Icons.play_arrow, color: Colors.green,),
-                  SizedBox(width: width*0.01,),
-                  const Text("Execute"),
-                ],
+      body: KeyboardListener(
+        focusNode: FocusNode(), 
+        onKeyEvent: (value) {
+          if (value.runtimeType == KeyDownEvent) {
+            String key = value.logicalKey.keyLabel.isNotEmpty ? value.logicalKey.keyLabel : value.logicalKey.debugName!;
+            if(key == "F5"){
+              viewModel.compile(state.inputCode.text);
+            }
+          }
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(
+              onPressed: (){
+                viewModel.compile( state.inputCode.text ) ;
+              },
+              icon:  IntrinsicWidth(
+                child:   Row(
+                  children: [
+                    const Icon(Icons.play_arrow, color: Colors.green,),
+                    SizedBox(width: width*0.01,),
+                    const Text("Execute"),
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(height: height*0.05,),
-          Expanded(
-            child: Padding(
-              padding:  EdgeInsets.symmetric(horizontal: width*0.02, vertical: height*0.02),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: width*0.7,
-                    child: InputBasic(
-                      maxLines: null,
-                      keyboard: TextInputType.multiline,
-                      labelTopPosition: true,
-                      controller: state.inputCode,
+            SizedBox(height: height*0.05,),
+            Expanded(
+              child: Padding(
+                padding:  EdgeInsets.symmetric(horizontal: width*0.02, vertical: height*0.02),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: width*0.7,
+                      child: InputBasic(
+                        maxLines: null,
+                        keyboard: TextInputType.multiline,
+                        labelTopPosition: true,
+                        controller: state.inputCode,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: width*0.02,),
-                  DataTable(
-                    columns: const [
-                      DataColumn(label: Text(HomeConstants.varsTitle)),
-                      DataColumn(label: Text(HomeConstants.terminalsTitle)),
-                    ],
-                    rows: [
-                      DataRow(
-                          cells: [DataCell(Text("VAR")), DataCell(Text("TERMINAL A"))   ]
-                      )
-                    ],
-                  )
-                ],
+                    SizedBox(width: width*0.02,),
+                    DataTable(
+                      columns: const [
+                        DataColumn(label: Text(HomeConstants.varsTitle)),
+                        DataColumn(label: Text(HomeConstants.terminalsTitle)),
+                      ],
+                      rows: [
+                        DataRow(
+                            cells: [DataCell(Text("VAR")), DataCell(Text("TERMINAL A"))   ]
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
